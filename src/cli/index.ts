ee52c73cf7471e6a53f2880fa51878284c7c7e11
Wenbo Yu <wenbo@open.store>
2022-11-14 08:22:41 -0800

    gzip tooling (#15)
    compress everything

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,7 +3,7 @@ import { log } from 'console'
 import { promises as fsPromise } from 'fs'
 import { makeS3Client, uploadFileToS3 } from './awsS3Client'
 import { v4 as uuidv4 } from 'uuid'
-
+import { execSync } from 'child_process'
 async function main() {
   log('Running: marketing-webflow-script CLI')
 
@@ -11,33 +11,45 @@ async function main() {
   const s3Client = makeS3Client()
   const pathToDist = path.resolve(__dirname, '..', '..', 'dist')
   const pathToEsbuild = path.resolve(pathToDist, 'esbuild')
-  const pathToLoader = path.resolve(pathToEsbuild, 'loader', 'index.js')
-  const pathToPackages = path.resolve(pathToEsbuild, 'packages', 'index.js')
+  const pathToCompressedLoader = path.resolve(
+    pathToEsbuild,
+    'loader',
+    'index.gz.js',
+  )
+  const pathToCompressedPackages = path.resolve(
+    pathToEsbuild,
+    'packages',
+    'index.gz.js',
+  )
   const pathToPackagesTags = path.resolve(pathToDist, 'tags.json')
+  const pathToCompressedPackagesTags = path.resolve(pathToDist, 'tags.gz.json')
   const tag = uuidv4()
 
-  const loaderUrl = await uploadFileToS3(
-    s3Client,
-    pathToLoader,
-    'loader/index.js', // Destination on S3 bucket.
-  )
+  const loaderUrl = await uploadFileToS3({
+    client: s3Client,
+    filePath: pathToCompressedLoader,
+    destPath: 'loader/index.gz.js',
+  })
   log(`Uploaded loader bundle to: '${loaderUrl}'`)
 
-  const packagesUrl = await uploadFileToS3(
-    s3Client,
-    pathToPackages,
-    `packages/${tag}.js`, // Destination on S3 bucket.
-  )
+  const packagesUrl = await uploadFileToS3({
+    client: s3Client,
+    filePath: pathToCompressedPackages,
+    destPath: `packages/${tag}.gz.js`,
+  })
   log(`Uploaded packages bundle to: '${packagesUrl}'`)
 
   log(`Tagging current release to version: '${tag}'`)
   log(`Writing to file: '${pathToPackagesTags}'`)
   await fsPromise.writeFile(pathToPackagesTags, JSON.stringify({ latest: tag }))
-  const tagsUrl = await uploadFileToS3(
-    s3Client,
-    pathToPackagesTags,
-    'loader/tags.json', // Destination on S3 bucket.
-  )
+  execSync(`gzip -c -9 ${pathToPackagesTags} > ${pathToCompressedPackagesTags}`)
+
+  const tagsUrl = await uploadFileToS3({
+    client: s3Client,
+    filePath: pathToCompressedPackagesTags,
+    destPath: 'loader/tags.gz.json',
+    contentType: 'application/json',
+  })
   log(`Uploaded tags to: '${tagsUrl}'`)
 
   log('Done')
--- src/loader/main.ts
+++ src/loader/main.ts
@@ -5,7 +5,7 @@ export default async function main() {
   let jsonResponse: Record<string, string>
 
   try {
-    const response = await fetch(`${STATIC_BUCKET_URL}loader/tags.json`, {
+    const response = await fetch(`${STATIC_BUCKET_URL}loader/tags.gz.json`, {
       cache: 'no-store', // Disable caching tags file for every request.
     })
     jsonResponse = await response.json()
@@ -19,5 +19,5 @@ export default async function main() {
     throw new Error('LOADER: missing package tag information')
   }
 
-  addScriptTag('AppPackage', `${STATIC_BUCKET_URL}packages/${tag}.js`)
+  addScriptTag('AppPackage', `${STATIC_BUCKET_URL}packages/${tag}.gz.js`)
 }
